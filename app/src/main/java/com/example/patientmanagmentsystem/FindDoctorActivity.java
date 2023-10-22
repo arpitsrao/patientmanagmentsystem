5533db5f484877199aecbf093f03e63608ee3c8b
arpitsrao <arpitsrao@gmail.com>
2023-10-22 23:21:53 +1300

    Main page development

--- app/src/main/java/com/example/patientmanagmentsystem/FindDoctorActivity.java
+++ app/src/main/java/com/example/patientmanagmentsystem/FindDoctorActivity.java
@@ -1,8 +1,11 @@
 package com.example.patientmanagmentsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FindDoctorActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class FindDoctorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_doctor);
+
+        CardView exit = findViewById(R.id.cardFDBack);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(FindDoctorActivity.this, HomeActivity.class));
+            }
+        });
+
+        CardView familyphysician = findViewById(R.id.cardLabTest);
+        familyphysician.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(FindDoctorActivity.this,DoctorDetailsActivity.class);
+                it.putExtra("title","Family Physician");
+                startActivity(it);
+            }
+        });
     }
 }
